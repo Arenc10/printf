9a688515eec426c22e0d76c5fdeaf5bd5b941a32
Arenc10 <arenc.palluqi@yahoo.com>
2022-03-12 06:09:28 -0800

    Implemented string logic

--- print_types.c
+++ print_types.c
@@ -7,6 +7,15 @@ void pr_char(va_list char_list)
 	c = va_arg(char_list, int);
 	_putchar(c);
 }
-void pr_string(va_list string_list);
+void pr_string(va_list string_list)
+{
+	int i;
+	char s;
+	for (i = 0; string_list[i] != '\0'; i++)
+	{
+		s = va_arg(string_list[i]);
+		_putchar(s);
+	}
+}
 void pr_float(va_list float_list);
 void pr_int(va_list int_list);
