a433de990a4d039e90fc049ec9e5254be0278447
KristiSeraj <kristiseraj2@gmail.com>
2022-03-12 04:57:20 -0800

    Print char function added

--- main.h
+++ main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdarg.h>
 /**
  * struct print_data - structure for printing data
  * @type: type of format
--- print_types.c
+++ print_types.c
@@ -1,2 +1,11 @@
 #include "main.h"
+#include <stdarg.h>
 
+void pr_char(va_list char_list)
+{
+	c = va_arg(char_list, int);
+	_putchar(c);
+}
+void pr_string(va_list string_list);
+void pr_float(va_list float_list);
+void pr_int(va_list int_list);
