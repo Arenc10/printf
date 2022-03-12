230c4117cb5c0e9ed2776b317e9f86c3fdfbf7fe
KristiSeraj <kristiseraj2@gmail.com>
2022-03-12 04:21:49 -0800

    Function prototypes and comment for structures added

--- main.h
+++ main.h
@@ -1,8 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct print_data - structure for printing data
+ * @type: type of format
+ * @print: function for printing
+ */
 typedef struct print_data
 {
 	char *type;
 	void (*print)(va_list arg_list);
 } print_data;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
 #endif
