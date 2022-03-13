c533e11a269fef39b5f4bc519bcf28a73dc07b03
KristiSeraj <kristiseraj2@gmail.com>
2022-03-13 05:11:26 -0700

    Function to print strings fixed

--- print_types.c
+++ print_types.c
@@ -1,20 +1,26 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
 
-void pr_char(va_list char_list)
+int pr_char(va_list char_list)
 {
 	char c;
 	c = va_arg(char_list, int);
 	_putchar(c);
+
+	return (1);
 }
-void pr_string(va_list string_list)
+int pr_string(va_list string_list)
 {
 	char *s;
 	int i;
 	s = va_arg(string_list, char*);
-	
+	if (s == NULL)
+		s = "NULL";
 	for (i = 0; s[i] != '\0'; i++)
 		_putchar(s[i]);
+
+	return (i);
 }
 void pr_float(va_list float_list);
 void pr_int(va_list int_list);
