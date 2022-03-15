9aaed3c2d28e6290ac91e98629860f707382ef9b
KristiSeraj <kristiseraj2@gmail.com>
2022-03-15 06:32:18 -0700

    Change

--- _printf.c
+++ _printf.c
@@ -5,23 +5,53 @@
 int _printf(const char *format, ...)
 {
 	va_list ag_list;
-	int i, j;
+	int i, j, flag;
+	int len = 0;
 	print_data p_func[] = {
 		{"s", pr_string},
 		{"c", pr_char},
 		{"i", pr_int},
-		{"d", pr_decimal}
+		{"d", pr_decimal}, 
+		{NULL, NULL}
 	};
 
 	va_start(ag_list, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (0);
 	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
-		j = 0;
-		while (j < 4 && (format[i] != *p_func[j].type))
-			j++;
-		if (j < 4)
-			p_func[j].print(ag_list);
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			j = 0;
+			flag = 0;
+			while (p_func[j].type != NULL)
+			{
+				if (format[i + 1] == p_func[j].type[0])
+				{
+					len = len + p_func[j].print(ag_list);
+					flag = 1;
+					i++;
+				}
+				j++;
+			}
+			if (flag == 0)
+			{
+				_putchar(format[i]);
+				len = len + 1;
+			}
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%');
+			i++;
+			len = len + 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			len = len + 1;
+		}
 		i++;
 	}
 	_putchar('\n');
